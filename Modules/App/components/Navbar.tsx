d0b045f33af4f4b5a20dace49cda6ebe914ec3eb
Sachintha Heshan <sachintha@leafycode.com>
2022-09-08 05:48:32 +0530

    feat: create NavBar

--- Modules/App/components/Navbar.tsx
+++ Modules/App/components/Navbar.tsx
@@ -1,1 +1,42 @@
-export const Navbar = () => <div>NavBar</div>;
+import Link from 'next/link';
+
+export const Navbar = () => (
+  <nav className="w-full bg-gray-900 flex justify-between items-center p-4">
+    <div className="flex justify-between items-end">
+      <img src="/images/derana-logo.png" alt="Derana logo" className="w-16" />
+      <div>
+        <span className="text-white font-bold text-4xl font-sans">ADA</span>
+        <span className="text-red-600 font-bold text-4xl">Derana</span>
+      </div>
+    </div>
+
+    <div className="flex">
+      <Link href="/">
+        <a className="text-gray-600 text-xl font-semibold  p-2">Sport</a>
+      </Link>
+      <Link href="/">
+        <a className="text-gray-50 text-xl font-semibold p-2">Tech</a>
+      </Link>
+      <Link href="/">
+        <a className="text-gray-600 text-xl font-semibold p-2">Travel</a>
+      </Link>{' '}
+      <Link href="/">
+        <a className="text-gray-600 text-xl font-semibold p-2">WorkLife</a>
+      </Link>
+      <Link href="/">
+        <a className="text-gray-600 text-xl font-semibold p-2">Gallery</a>
+      </Link>
+    </div>
+
+    <div className="flex justify-center items-center  ">
+      <img
+        src="/images/user-avatar.png"
+        alt="User Avatar"
+        className="w-11 m-2 p-0.5 border-2 border-sky-800 rounded-full"
+      />
+      <button type="button" className="text-white bg-gray-800 hover:bg-gray-600 p-2 rounded-md border border-gray-700">
+        Sign In
+      </button>
+    </div>
+  </nav>
+);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Navbar } from '../Modules/App/components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <main>
-      <Navbar />
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>Derana News</title>
+      </Head>
+      <main>
+        <Navbar />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
 
