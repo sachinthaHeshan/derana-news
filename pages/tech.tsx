787648c75886fe92a62d879dc7d7e22d215df90d
Sachintha Heshan <sachintha@leafycode.com>
2022-09-09 16:30:21 +0530

    feat: create sport, tech, gallery pages

---
+++ pages/tech.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Tech: NextPage = () => (
+  <div className="   flex justify-center">
+    <img src="/images/cricket.webp" className="w-full h-screen-7/10 object-cover brightness-50" alt="" />
+  </div>
+);
+
+export default Tech;
