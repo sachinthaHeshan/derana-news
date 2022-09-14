59531fd47da8c97575e3291c4b8bdcb072af5708
Sachintha Heshan <sachintha@leafycode.com>
2022-09-15 02:56:49 +0530

    style: change styles in category selector

--- modules/shared/components/CategorySelector.tsx
+++ modules/shared/components/CategorySelector.tsx
@@ -9,6 +9,7 @@ export function CategorySelector() {
 
   const isCategoryPage = router?.asPath?.includes('category');
 
+  const categories = ['Tech', 'Sport', 'Gallery', 'Education', 'Local'];
   return (
     <div
       className={`flex items-center justify-center   ${
@@ -32,73 +33,21 @@ export function CategorySelector() {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-black border border-gray-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="px-1 py-1 ">
-              <Menu.Item>
+          <Menu.Items className="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-black border border-gray-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none p-2">
+            {categories?.map((category) => (
+              <Menu.Item key={category}>
                 {({ active }) => (
                   <button
                     type="button"
                     className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-white'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      active ? 'bg-lava-red text-white bg-opacity-60' : 'text-white'
+                    } group border-none   flex w-full items-center rounded-md px-2 py-2 text-sm`}
                   >
-                    Edit
+                    {category}
                   </button>
                 )}
               </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="button"
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-white'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Duplicate
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-            <div className="px-1 py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="button"
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-white'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Archive
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="button"
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-white'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Move
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-            <div className="px-1 py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="button"
-                    className={`${
-                      active ? 'bg-red-500 text-white' : 'text-white'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    Delete
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
+            ))}
           </Menu.Items>
         </Transition>
       </Menu>
